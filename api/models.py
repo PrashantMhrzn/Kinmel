4bac05a6cda59ceecec97cc804bf839909601262
PrashantMhrzn <prashantmaharjan5@gmail.com>
2025-07-02 00:03:03 +0545

    Added all Models | Migrations Completed

--- api/models.py
+++ api/models.py
@@ -13,27 +13,28 @@ class User(AbstractUser):
     phone = m.CharField(max_length=15, blank=True, null=True)
     address = m.TextField(blank=True, null=True)
 
-
 class SellerProfile(m.Model):
+    # One user(who is a seller) can only have one Seller Profile
     user = m.OneToOneField(User, on_delete=m.CASCADE)
-    company_name = m.CharField()
+    company_name = m.CharField(max_length=100)
     verified = m.BooleanField(default=False)
 
 class Category(m.Model):
-    name = m.CharField()
-    description = m.TextField()
+    name = m.CharField(max_length=100, unique=True)
+    description = m.TextField(blank=True)
 
 class Product(m.Model):
-    name = m.CharField()
+    name = m.CharField(max_length=100)
     description = m.TextField()
-    price = m.PositiveIntegerField()
-    category = m.ForeignKey(Category, on_delete=m.PROTECT)
-    seller = m.ForeignKey(User, on_delete=m.CASCADE)
+    price = m.DecimalField(max_digits=10, decimal_places=2)
+    category = m.ForeignKey(Category, on_delete=m.SET_NULL, null=True)
+    seller = m.ForeignKey(User, on_delete=m.CASCADE, limit_choices_to={'role': 'seller'})
+    posted_at = m.DateTimeField(auto_now_add=True)
 
 class SellerInventory(m.Model):
-    seller = m.OneToOneField(User, on_delete=m.CASCADE)
+    seller = m.OneToOneField(User, on_delete=m.CASCADE, limit_choices_to={'role': 'seller'})
     product = m.ForeignKey(Product, on_delete=m.CASCADE)
-    stock_quantity = m.IntegerField()
+    stock_quantity = m.PositiveIntegerField()
 
 class Cart(m.Model):
     user = m.OneToOneField(User, on_delete=m.CASCADE, limit_choices_to={'role': 'customer'})
@@ -55,22 +56,32 @@ class CartItems(m.Model):
         unique_together = ('cart', 'product')
 
 class Order(m.Model):
-    customer = m.ForeignKey(User, on_delete=m.CASCADE)
-    total_price = m.IntegerField()
-    status = m.CharField()
-    created_at = m.DateTimeField()
-    updated_at = m.DateTimeField()
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('processing', 'Processing'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled'),
+    )
+    # Only customer can make a order
+    customer = m.ForeignKey(User, limit_choices_to={'role': 'customer'}, on_delete=m.CASCADE)
+    total_price = m.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    status = m.CharField(choices=STATUS_CHOICES, default='pending')
+    created_at = m.DateTimeField(auto_now_add=True)
+    updated_at = m.DateTimeField(auto_now=True)
 
 class OrderItem(m.Model):
     order = m.ForeignKey(Order, on_delete=m.CASCADE, related_name='items')
     product = m.ForeignKey(Product, on_delete=m.CASCADE)
     quantity = m.PositiveIntegerField()
-    purchase_price = m.IntegerField()
+    purchase_price = m.DecimalField(max_digits=10, decimal_places=2)
 
 class Delivery(m.Model):
+    # One order can only be delivered once
     order = m.OneToOneField(Order, on_delete=m.CASCADE)
-    delivery_person = m.ForeignKey(User, on_delete=m.CASCADE)
-    status = m.CharField()
+    delivery_person = m.ForeignKey(User, limit_choices_to={'role': 'delivery'},on_delete=m.CASCADE)
+    # Choices are already declared in Order Model
+    status = m.CharField(choices=Order.STATUS_CHOICES)
     shipped_at = m.DateTimeField(null=True, blank=True)
     delivered_at = m.DateTimeField(null=True, blank=True)
 
@@ -78,4 +89,4 @@ class Notification(m.Model):
     user = m.ForeignKey(User, on_delete=m.CASCADE)
     mesage = m.TextField()
     seen = m.BooleanField(default=False)
-    created_at = m.DateTimeField()
+    created_at = m.DateTimeField(auto_now_add=True)
