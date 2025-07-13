ae4c4c4ae0c912cad981f3e6d5b337eaa9adacb0
PrashantMhrzn <prashantmaharjan5@gmail.com>
2025-07-13 17:07:43 +0545

    Added views respectively using ModelViewSet

--- api/views.py
+++ api/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
-# Views for all the models using ModelViewSet
-# .
+class SellerProfileView(ModelViewSet):
+    queryset = SellerProfile.objects.all()
+    serializer_class = SellerProfileSerializer
+
+class CategoryView(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductView(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class SellerInventoryView(ModelViewSet):
+    queryset = SellerInventory.objects.all()
+    serializer_class = SellerInventorySerializer
+
+class CartView(ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class OrderView(ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class DeliveryView(ModelViewSet):
+    queryset = Delivery.objects.all()
+    serializer_class = DeliverySerializer
+
+class NotificationView(ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
