2d7b9a67a1e6ee7e7c5a9e197211e440312efdd4
Prashant Maharjan <prashantmaharjan5@gmail.com>
2025-10-27 13:12:20 +0545

    Fixed enviornment variables in database setup (#1)

--- project/settings.py
+++ project/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kinmel', # name of your db
-        'USER': 'postgres', # your username
-        'PASSWORD': 'root', # your password
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
